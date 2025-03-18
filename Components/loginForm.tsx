f254805c449cbf31422110e332dc6fdefefb7582
BettyyN <betelhemnegash10@gmail.com>
2025-03-18 10:29:34 +0300

    login functionality added

--- Components/loginForm.tsx
+++ Components/loginForm.tsx
@@ -2,19 +2,23 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { signIn } from "next-auth/react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 // Zod schema for login validation
 const loginSchema = z.object({
-  phone: z
+  phoneNumber: z
     .string()
-    .min(1, "Phone number is required")
-    .regex(/^\d+$/, "Invalid phone number"),
+    .min(1, "phoneNumber  is required")
+    .regex(/^(\+251[79]\d{8}|0[79]\d{8})$/, "Invalid phone number format"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 type LoginFormData = z.infer<typeof loginSchema>;
 
 export default function LoginForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -22,10 +26,28 @@ export default function LoginForm() {
   } = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
   });
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues:{
+      phoneNumber:'',
+      password:'',
+    }
+  });
 
-  const onSubmit = (data: LoginFormData) => {
-    console.log("Login Data:", data);
-    alert("Login Successful!");
+  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+    const signInData = await signIn('credentials',{
+      phoneNumber: values.phoneNumber,
+      password: values.password,
+      redirect: false,
+    });
+    console.log(signInData)
+    if(signInData?.error){
+      toast.error(signInData.error);
+      console.log(signInData.error)
+    }
+    else{
+      router.push('/home')
+    }
   };
 
   return (
@@ -39,13 +61,13 @@ export default function LoginForm() {
           onSubmit={handleSubmit(onSubmit)}
           className="space-y-4 grid grid-cols-1 gap-6 text-black"
         >
-          {/* Phone Number */}
+          {/* phoneNumber Number */}
           <div className="relative">
             <input
               type="tel"
-              {...register("phone")}
+              {...register("phoneNumber")}
               className={`w-full border rounded-md px-3 pt-5 pb-2 peer focus:outline-none focus:ring-2 ${
-                errors.phone
+                errors.phoneNumber
                   ? "border-red-400 focus:ring-red-400"
                   : "border-gray-300 focus:ring-purple-500"
               }`}
@@ -53,9 +75,9 @@ export default function LoginForm() {
             <label className="absolute left-3 top-0 bg-white px-1 text-sm text-gray-600 peer-focus:text-purple-600 peer-focus:-translate-y-3 transition-all">
               Phone Number
             </label>
-            {errors.phone && (
+            {errors.phoneNumber && (
               <p className="text-red-500 text-xs mt-1">
-                {errors.phone?.message}
+                {errors.phoneNumber?.message}
               </p>
             )}
           </div>
