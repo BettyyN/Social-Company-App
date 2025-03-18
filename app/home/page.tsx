ddef6183b1838831f53712692d4a1ba2468e05fa
BettyyN <betelhemnegash10@gmail.com>
2025-03-18 10:29:13 +0300

    commented the "use client" line because it is causing error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,30 @@
-"use client";
+// "use client";
 
+import { getServerSession } from "next-auth";
 import Drawer from "../../Components/drawer";
 import PostCard from "../../Components/postCard";
+import { authOptions } from "@/lib/auth";
 
-export default function page() {
+export default async function page() {
+  const session =await getServerSession(authOptions);
+  console.log(session);
+  if (session?.user){
+    return (
+        <div className="flex flex-row">
+          <div>
+            <Drawer />
+          </div>
+          <div>
+            <PostCard />
+            <h2>Welcome back {session?.user.phoneNumber}</h2>
+          </div>
+        </div>
+    );
+  }
   return (
     <>
       <div className="flex flex-row">
-        <Drawer />
-        <PostCard />
-      </div>
+        <h1> please logion.</h1>        </div>
     </>
   );
 }
