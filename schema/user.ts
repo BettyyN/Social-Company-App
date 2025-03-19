2099d161cf95e396e2247a148dd2090d8e56d3bc
BettyyN <betelhemnegash10@gmail.com>
2025-03-19 12:28:17 +0300

    password validation

--- schema/user.ts
+++ schema/user.ts
@@ -5,13 +5,22 @@ export const user = z
     firstName: z.string().min(2, "First Name must be at least 2 characters"),
     lastName: z.string().min(2, "Last Name must be at least 2 characters"),
     email: z.string().email("Invalid email address"),
-    phoneNumber: z.string().min(10, "Phone number must be at least 10 digits").regex(/^(\+251[79]\d{8}|0[79]\d{8})$/, "Invalid phone number format")
+    phoneNumber: z
+      .string()
+      .min(10, "Phone number must be at least 10 digits")
+      .regex(/^(\+251[79]\d{8}|0[79]\d{8})$/, "Invalid phone number format")
 
-    .max(13 , "phone number not correct"),
+      .max(13, "phone number not correct"),
     // baptismalName: z
     //   .string()
     //   .min(2, "Baptismal Name must be at least 2 characters"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .regex(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+        "Password must contain at least one letter and one number"
+      ),
     confirmPassword: z
       .string()
       .min(6, "Confirm Password must be at least 6 characters"),
