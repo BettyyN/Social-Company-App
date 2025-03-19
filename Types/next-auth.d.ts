49a7f10bb3191879a8410944a8e75705f3b96cf4
BettyyN <betelhemnegash10@gmail.com>
2025-03-19 12:28:37 +0300

    uset interface

--- Types/next-auth.d.ts
+++ Types/next-auth.d.ts
@@ -2,16 +2,22 @@ import NextAuth from "next-auth";
 import { string } from "zod";
 
 declare module "next-auth"{
-    interface User{
-     phoneNumber:string;
+    interface User {
+      phoneNumber: string;
+      firstName: string;
+      lastName: string;
     }
-    interface Session{
-    user: User & {
-     phoneNumber:string;
-    }
-    token:{
-       phoneNumber:string;
-    }
+    interface Session {
+      user: User & {
+        phoneNumber: string;
+        firstName: string;
+        lastName: string;
+      };
+      token: {
+        phoneNumber: string;
+        firstName: string;
+        lastName: string;
+      };
     }
    }
 
