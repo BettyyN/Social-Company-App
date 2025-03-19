136e7c7eacbe108e76c4a64d4c006a63615d3a30
BettyyN <betelhemnegash10@gmail.com>
2025-03-19 12:22:52 +0300

    add session data

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,27 +4,37 @@ import { getServerSession } from "next-auth";
 import Drawer from "../../Components/drawer";
 import PostCard from "../../Components/postCard";
 import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import HomeHeader from "@/Components/homeheader";
+import User from "@/Components/user";
 
 export default async function page() {
   const session =await getServerSession(authOptions);
   console.log(session);
   if (session?.user){
     return (
-        <div className="flex flex-row">
-          <div>
+      <>
+        <div className="flex flex-row h-screen">
+          <div className="flex">
             <Drawer />
           </div>
-          <div>
+          <div className="flex-1 overflow-y-auto transition-margin duration-300">
+            <HomeHeader />
             <PostCard />
-            <h2>Welcome back {session?.user.phoneNumber}</h2>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <PostCard />
+            <h2>Welcome back {session?.user.firstName}</h2>
+
+            <h2> client</h2>
+            <User/>
+            <h2>Server</h2>
+            {JSON.stringify(session)}
           </div>
         </div>
+      </>
     );
   }
-  return (
-    <>
-      <div className="flex flex-row">
-        <h1> please logion.</h1>        </div>
-    </>
-  );
+  return redirect("/auth/login");
 }
