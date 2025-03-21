32e8ae33e7c0cc42ca6ed2841c470a92ad47e045
BettyyN <betelhemnegash10@gmail.com>
2025-03-21 10:38:58 +0300

    user schema file updated

--- schema/userSchema.ts
+++ schema/userSchema.ts
@@ -1,10 +1,35 @@
 import { z } from "zod";
 
-export const userSchema = z.object({
-  firstName: z.string().min(1, "First name is required"),
-  lastName: z.string().min(1, "Last name is required"),
-  email: z.string().email("Invalid email format"),
-  phoneNumber: z.string().optional(), // Optional field
-  positionId: z.number().int().positive().optional(), // Must be a positive integer if provided
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-});
+export const userSchema = z
+  .object({
+    firstName: z.string().min(2, "First Name must be at least 2 characters"),
+    lastName: z.string().min(2, "Last Name must be at least 2 characters"),
+    email: z.string().email("Invalid email address").optional(),
+    phoneNumber: z
+      .string()
+      .min(10, "Phone number must be at least 10 digits")
+      .regex(
+        /^(\+251[79]\d{8}|0[79]\d{8})$/,
+        "Invalid phone number format. Use +251... or 09/07..."
+      )
+      .max(13, "Phone number must be maximum 13 digits"),
+    baptismalName: z
+      .string()
+      .min(2, "Baptismal Name must be at least 2 characters")
+      .optional(),
+    role: z.enum(["STUDENT", "TEACHER", "ADMIN", "OTHER"]) ,
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .regex(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+        "Password must contain at least one letter and one number"
+      ),
+    confirmPassword: z.string().min(1, "Confirm Password is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type UserFormData = z.infer<typeof userSchema>;
