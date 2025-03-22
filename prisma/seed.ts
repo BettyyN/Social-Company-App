46c634862f51cc1067a3a49e53e833ef8eca9632
BettyyN <betelhemnegash10@gmail.com>
2025-03-22 11:56:12 +0300

    seed file updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,14 +2,34 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  // Seed default roles
+  await prisma.$executeRaw`DELETE FROM "User";`;
+
+  // Reset the ID sequence
+  await prisma.$executeRaw`ALTER SEQUENCE "User_id_seq" RESTART WITH 1;`;
+
   await prisma.role.createMany({
     data: [{ role: "STUDENT" }, { role: "TEACHER" }, { role: "ADMIN" }],
     skipDuplicates: true,
   });
+const adminRole = await prisma.role.findUnique({
+  where: { role: "ADMIN" },
+});
 
+  // Insert a sample user
+  await prisma.user.create({
+    data: {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@example.com",
+      password: "hashedpassword",
+      phoneNumber: "123456789",
+      roleId: adminRole.id,
+    },
+  });
   console.log("Default roles seeded successfully");
 }
+
+ 
 
 main()
   .catch((e) => {
