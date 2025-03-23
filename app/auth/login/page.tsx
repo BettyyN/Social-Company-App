94fbb15b0851b57ea0b727039b6f04f2e7a9f18c
BettyyN <betelhemnegash10@gmail.com>
2025-03-23 16:14:55 +0300

    config login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,17 +4,17 @@ import LoginForm from "@/Components/loginForm";
 export default function page() {
   return (
     <>
-      <div className="bg-gradient-to-bl from-[#6000DA] to-[#A270FF] ">
+      <div className="bg-slate-100 min-h-screen ">
         <Header />
-        <div className="min-h-screen flex lg:flex-row flex-col">
+        <div className="flex lg:flex-row flex-col  items-center mt-20">
           <div className="lg:w-1/3 w-full">
             <img
               src="/login.png"
               alt="signup"
-              className="lg:absolute lg:w-[700px] lg:h-[700px] lg:top-1/2 lg:left-1/6 transform lg:-translate-x-1/3 lg:-translate-y-1/2"
+              className="lg:absolute lg:w-[500px] lg:h-[500px] lg:top-1/2 lg:left-1/6 transform lg:-translate-x-1/3 lg:-translate-y-1/2"
             />
           </div>
-          <div className="lg:w-2/3 w-full bg-white rounded-tl-3xl rounded-bl-3xl">
+          <div className="lg:w-1/3 w-full bg-secondary rounded-tl-3xl rounded-bl-3xl shadow-primary shadow-lg">
             <LoginForm />
           </div>
         </div>
