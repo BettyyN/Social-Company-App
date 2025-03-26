b3d18f01368ee26503c84cd162ef1cfc1f1194a1
BettyyN <betelhemnegash10@gmail.com>
2025-03-27 00:20:16 +0300

    new style for drawer

--- Components/drawer.tsx
+++ Components/drawer.tsx
@@ -9,9 +9,9 @@ export default function drawer() {
   return (
     <>
       <div
-        className={`h-screen bg-[#F4F0FF] text-white ${
+        className={`h-screen bg-primary text-black ${
           isOpen ? "w-64" : "w-20"
-        } transition-all duration-300 p-4 flex flex-col relative`}
+        } transition-all duration-300 flex flex-col relative`}
       >
         {/* Toggle Button */}
         <button
@@ -22,41 +22,30 @@ export default function drawer() {
         </button>
 
         {/* Navigation */}
-        <nav className="flex flex-col flex-grow mt-6 space-y-2 text-black text-sm">
-          {[
-            {
-              label: "General",
-              icon: <FiMessageSquare size={24} />,
-              path: "/general",
-            },
-            {
-              label: "Messages",
-              icon: <FiMessageSquare size={24} />,
-              path: "/messages",
-            },
-
-            { label: "Teams", icon: <FiUsers size={24} />, path: "/teams" },
-            {
-              label: "Events",
-              icon: <FiSettings size={24} />,
-              path: "/events",
-            },
-            {
-              label: "Settings",
-              icon: <FiSettings size={24} />,
-              path: "/settings",
-            },
-          ].map((item, index) => (
-            <Link
-              href={item.path}
-              key={index}
-              className="flex items-center gap-3 p-3 rounded-md transition-all"
-            >
-              {item.icon}
-              {isOpen && <span className="text-lg">{item.label}</span>}
-            </Link>
-          ))}
-        </nav>
+        <div className="flex flex-col flex-grow mt-6 space-y-2 text-sm">
+          <ul className="flex flex-col w-full pl-5">
+            <li className="flex w-full p-3 hover:bg-secondary rounded transition-colors cursor-pointer h-10 bg-secondary">
+              <FiMessageSquare size={24} className="text-white" />
+              <h1 className="text-white">General</h1>
+            </li>
+            <li className="flex w-full p-3 hover:bg-secondary rounded-xl transition-colors cursor-pointer h-15 bg-white">
+              <FiMessageSquare size={24} />
+              <h1>Messages</h1>
+            </li>
+            <li className="flex w-full p-3 hover:bg-secondary rounded-xl transition-colors cursor-pointer h-15 bg-white">
+              <FiMessageSquare size={24} />
+              <h1>Events</h1>
+            </li>
+            <li className="flex w-full p-3 hover:bg-secondary rounded-xl transition-colors cursor-pointer h-15 bg-white">
+              <FiMessageSquare size={24} />
+              <h1>Teams</h1>
+            </li>
+            <li className="flex w-full p-3 hover:bg-secondary rounded-xl transition-colors cursor-pointer h-15 bg-white">
+              <FiMessageSquare size={24} />
+              <h1>Settings</h1>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
