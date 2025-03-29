8f442f5c8f0081aad1902f3d7afc0a45f9c894aa
BettyyN <betelhemnegash10@gmail.com>
2025-03-29 11:16:27 +0300

    passing user data to the header compoonent

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,20 +8,23 @@ import { redirect } from "next/navigation";
 import HomeHeader from "@/Components/homeheader";
 import User from "@/Components/user";
 
+
 export default async function page() {
   const session =await getServerSession(authOptions);
   console.log(session);
   if (session?.user){
+    // const userName=(session?.user.firstName+" "+session?.user.lastName).toString();
+    const userName=(session?.user.firstName+" ").toString();
     return (
       <>
-        <div className="flex flex-row h-screen">
+        <div className="flex flex-row h-screen bg-[#eff7ff]">
           <div className="flex">
             <Drawer />
           </div>
           <div className="flex-1 overflow-y-auto transition-margin duration-300">
-            <HomeHeader />
+            <HomeHeader userName={userName} />
+            {/* <h2 className=" m-5 justify-center">Welcome back {session?.user.firstName}</h2> */}
             <PostCard />
-            <h2>Welcome back {session?.user.firstName}</h2>
           </div>
         </div>
       </>
