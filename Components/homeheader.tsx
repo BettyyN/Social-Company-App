b1b2a0e9fa00097688acecdfc9f327af59ee2da6
BettyyN <betelhemnegash10@gmail.com>
2025-03-29 11:16:56 +0300

    header style updated

--- Components/homeheader.tsx
+++ Components/homeheader.tsx
@@ -7,18 +7,22 @@ import { signOut } from "next-auth/react";
 import { useState } from "react";
 import Profile from "./Profile";
 
-export default function HomeHeader() {
+export default function HomeHeader({ userName }: { userName: string }) {
   return (
     <>
-      <header className="shadow-sm bg-[#F4F0FF] text-primary opacity-80 sticky top-0 z-50">
-        <nav className="container mx-auto flex items-center  p-2 justify-center">
+      <header className="shadow-sm bg-[#F4F0FF] text-primary opacity-80 sticky top-0 z-50 md:mx-5 rounded-2xl flex">
+        <div className="text-sm w-1/3 md:w-1/4 p-5 md:text-lg">
+          <span className="flex">Selam, {userName}</span>
+          {/* <span className="flex"></span> */}
+        </div>
+        <div className="container flex items-center  pl-40  py-2">
           {/* Desktop Menu */}
-          <div className="flex flex-row space-x-10 text-primary">
-            <div className="pt-2 text-primary">
-              <MessageCircle size={28} />
+          <div className="flex flex-row md:space-x-10 text-primary space-x-3">
+            <div className="pt-2">
+              <MessageCircle className="w-5 h-5 md:w-7 md:h-7" />
             </div>
             <div className="pt-2">
-              <Bell size={28} />
+              <Bell className="w-5 h-5 md:w-7 md:h-7" />
             </div>
             <div className="w-12 rounded-full">
               <Link href={"/profile"}>
@@ -28,7 +32,7 @@ export default function HomeHeader() {
                 />
               </Link>
             </div>
-            <div>
+            <div className="text-">
               <button
                 className="pt-3 underline"
                 onClick={() =>
@@ -42,7 +46,7 @@ export default function HomeHeader() {
               </button>
             </div>
           </div>
-        </nav>
+        </div>
       </header>
     </>
   );
