59f43214fbdee72d4e01de5a8aa2e843ce098a98
BettyyN <betelhemnegash10@gmail.com>
2025-03-29 11:16:40 +0300

    updated the drawer

--- Components/drawer.tsx
+++ Components/drawer.tsx
@@ -10,25 +10,27 @@ export default function drawer() {
     <>
       <div
         className={`h-screen bg-[#645CBB] ${
-          isOpen ? "w-64" : "w-20"
+          isOpen ? "w-64" : "w-16 bg-transparent shadow-2xl shadow-primary"
         } transition-all duration-300 flex flex-col relative`}
       >
         {/* Toggle Button */}
         <button
-          className="text-primary p-2 self-end focus:outline-none"
+          className="text-primary p-5 self-end focus:outline-none flex "
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
         </button>
         {/* Navigation */}
-        <div className="flex flex-col flex-grow mt-20 space-y-2 text-sm">
-          <ul className="flex flex-col w-full pl-5">
-            <li className="flex w-full p-7 hover:bg-secondary rounded-4xl rounded-r-none transition-colors cursor-pointer h-10 bg-[#c0deff] gap-2 items-center">
-              <FiMessageSquare size={24} className="text-white pt-1" />
-              <h1 className="text-white ">General</h1>
-            </li>
-          </ul>
-        </div>
+        {isOpen && (
+          <div className="flex flex-col flex-grow mt-16 space-y-2 text-sm text-[#162655]">
+            <ul className="flex flex-col w-full pl-5">
+              <li className="flex w-full p-7 hover:bg-secondary rounded-4xl rounded-r-none rounded-e-xl transition-colors cursor-pointer h-10  gap-2 items-center hover:bg-[#eff7ff]">
+                <FiMessageSquare size={24} className=" pt-1" />
+                <h1 className=" text-lg ">General</h1>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
