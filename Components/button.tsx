24c272954b8e0a8041d4ccf30cadba419cd84e61
BettyyN <betelhemnegash10@gmail.com>
2025-04-09 23:49:48 +0300

    button component

---
+++ Components/button.tsx
@@ -0,0 +1,10 @@
+
+function Button() {
+  return (
+    <div>
+      <button className="w-32 h-14 border-primary border-2 bg-primary hover:bg-primary hover:border-white shadow-sm shadow-primary"></button>
+    </div>
+  );
+}
+
+export default Button
