4cc757b21dddb03ffb3355aa3d4ec5d5b71312c5
BettyyN <betelhemnegash10@gmail.com>
2025-04-09 23:49:31 +0300

    config profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,20 @@
 import ProfileCover from "@/Components/profileCover";
+import ProfileAbout from "@/Components/profileAbout";
+import ProfilePost from "@/Components/profilePost";
 
 function page() {
   return (
-   <ProfileCover/>
+    <>
+      <ProfileCover />
+      <div className="flex gap-3 m-3">
+        <div className="ml-24">
+          <ProfileAbout />
+        </div>
+        <div className="pl-5 py-3 pr-24">
+          <ProfilePost />
+        </div>
+      </div>
+    </>
   );
 }
 
