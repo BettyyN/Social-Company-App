d02290f3d3209717d77b151dc456c683a07326f5
BettyyN <betelhemnegash10@gmail.com>
2025-04-15 11:06:28 +0300

    role permission back

--- app/api/rolePermission/route.ts
+++ app/api/rolePermission/route.ts
@@ -3,16 +3,32 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try{
-    const permissions = await db.rolePermission.findMany({
+    const rolePermissions = await db.rolePermission.findMany({
         orderBy:{
-            permissionId:"asc"
+            roleId:"asc"
         },
         include:{
             permission:true,
             role:true
         },
     });
-    return NextResponse.json(permissions,{status:200})
+    const grouped = new Map();
+    for (const item of rolePermissions){
+        const roleId = item.roleId;
+        if(!grouped.has(roleId)){
+            grouped.set(roleId,{
+                roleId: item.roleId,
+                roleName: item.role.roleName,
+                permissions:[],
+            });
+        }
+        grouped.get(roleId).permissions.push({
+            permissionId: item.permissionId,
+            permissionName: item.permission.permissionName,
+            description: item.permission.description,
+        });
+    }
+    return NextResponse.json(Array.from(grouped.values()), { status: 200 });
   }
   catch(error){
    console.log(error)
@@ -57,4 +73,70 @@ return NextResponse.json(newPermission,{status:201})
         )
     }
 }
+
+export async function PUT(req: NextRequest) {
+  const body = await req.json();
+  const { roleId, permissionId } = body;
+  if (!roleId || !permissionId) {
+    return NextResponse.json(
+      { error: "Missing roleId or permissionId" },
+      { status: 400 }
+    );
+  }
+  try {
+    const updatePermission = await db.rolePermission.update({
+      where: {
+        roleId_permissionId: {
+          roleId: Number(roleId),
+          permissionId: Number(permissionId),
+        },
+      },
+      data: {
+        roleId: Number(roleId),
+        permissionId: Number(permissionId),
+      },
+    });
+    return NextResponse.json(updatePermission, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const roleId = Number(searchParams.get("roleId"));
+  const permissionId = Number(searchParams.get("permissionId"));
+  if (!roleId || !permissionId) {
+    return NextResponse.json(
+      { error: "Mission roleId or permissionId" },
+      { status: 400 }
+    );
+  }
+  try {
+    await db.rolePermission.delete({
+      where: {
+        roleId_permissionId: {
+          roleId,
+          permissionId,
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Permission removed from role successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
 
