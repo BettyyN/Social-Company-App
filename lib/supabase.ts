23e1d1cb5f703a335dd701ef82989ec40e498add
BettyyN <betelhemnegash10@gmail.com>
2025-04-16 11:52:38 +0300

    supabase client created

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,14 @@
-// lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
+// Use anon key for client-side operations
+const supabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_KEY! // Anon key for client-side
 );
 
-export { supabase };
+const supabaseServer = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // Service role key for server-side operations
+);
+
+export { supabaseClient, supabaseServer };
