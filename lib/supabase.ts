f1574189295fe6bf8a35c80077d4bdeb5d5be953
BettyyN <betelhemnegash10@gmail.com>
2025-04-17 09:29:58 +0300

    updated the subabase client to use service role

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Use anon key for client-side operations
 const supabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY! // Anon key for client-side
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
-const supabaseServer = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // Service role key for server-side operations
-);
+export { supabaseClient };
 
-export { supabaseClient, supabaseServer };
+
+
+// // Use anon key for client-side operations
+// const supabaseClient = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_KEY! // Anon key for client-side
+// );
+
+// const supabaseServer = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY! // Service role key for server-side operations
+// );
