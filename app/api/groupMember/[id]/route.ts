3e07c3ff6ea2fbab212f2ccf7bb26ff0fe5c1491
BettyyN <betelhemnegash10@gmail.com>
2025-04-25 20:37:01 +0300

    group member api updated

--- app/api/groupMember/[id]/route.ts
+++ app/api/groupMember/[id]/route.ts
@@ -33,3 +33,68 @@ export async function DELETE(req: NextRequest) {
     );
   }
 }
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const groupId = Number(params.id);
+
+    if (isNaN(groupId)) {
+      return NextResponse.json(
+        { message: "Invalid group ID" },
+        { status: 400 }
+      );
+    }
+
+    const group = await db.group.findUnique({
+      where: {
+        groupId,
+        deletedAt: null,
+      },
+      select: {
+        groupId: true,
+        groupName: true,
+        groupDescription: true,
+        profilePicture: true,
+        members: {
+          include: {
+            user: {
+              select: {
+                userId: true,
+                firstName: true,
+                lastName: true,
+                phoneNumber: true,
+                baptismalName: true,
+                profilePicture: true,
+                createdAt: true,
+                roleId: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!group) {
+      return NextResponse.json({ message: "Group not found" }, { status: 404 });
+    }
+
+    const result = {
+      groupId: group.groupId,
+      groupName: group.groupName,
+      groupDescription: group.groupDescription,
+      profilePicture: group.profilePicture,
+      members: group.members.map((m) => m.user),
+    };
+
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching group members:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
