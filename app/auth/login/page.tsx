5efbcccd15dc5213c12fdac79df09301a3945191
BettyyN <betelhemnegash10@gmail.com>
2025-04-28 20:14:14 +0300

    config

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -14,7 +14,7 @@ export default function page() {
               className="lg:absolute lg:w-[500px] lg:h-[500px] lg:top-1/2 lg:left-1/6 transform lg:-translate-x-1/3 lg:-translate-y-1/2"
             />
           </div>
-          <div className="lg:w-1/3 w-full bg-secondary rounded-tl-3xl rounded-bl-3xl shadow-primary shadow-lg">
+          <div className="lg:w-1/3 w-full rounded-tl-3xl rounded-bl-3xl shadow-primary shadow-lg">
             <LoginForm />
           </div>
         </div>
