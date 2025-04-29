b5bd667e6f1dee5cbfb63b4c3d4292f970e5e3a2
BettyyN <betelhemnegash10@gmail.com>
2025-04-29 21:05:09 +0300

    config first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,28 @@
+"use client";
 import Image from "next/image";
-import heroBackground from "../public/nnn.jpg";
+import heroBackground from "../public/nature.jpg";
+import rose from "../public/r1.jpg";
 import Header from "../Components/header";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const handleNavigation = (path: string) => (e: React.MouseEvent) => {
+    e.preventDefault();
+    router.push(path);
+  };
   return (
     <main className="h-screen w-full overflow-hidden relative bg-[#F5F7FA]">
-      {/* Background image
-      <div className="absolute inset-0 z-0">
-        <Image
-          src={heroBackground}
-          alt="Begenra Institute Background"
-          fill
-          className="object-cover"
-          priority
-        />
-      </div> */}
-
-      {/* Overlay + Header */}
       <div className="absolute inset-0 z-10 ">
         <Header />
       </div>
 
-      {/* Hero content */}
-      <div className="relative  h-full flex text-neutral-content w-full items-center gap-6">
-        <div className="w-2/3 p-28">
-          <h1 className="mb-5 md:text-5xl text-2xl font-bold text-[#7300ff]">
+      <div className="relative  h-full flex text-neutral-content w-full items-center gap-6 flex-col md:flex-row justify-center ">
+        <div className="md:w-2/5 p-5  w-full">
+          <h1 className="mb-5 md:text-3xl text-2xl font-bold text-[#7300ff]">
             Selam,
           </h1>
-          <h2 className="mb-5 md:text-5xl font-bold">
+          <h2 className="mb-5 md:text-2xl font-bold">
             Kidus Yared Yebegena Maseltegna
           </h2>
           <p className="mb-5 text-justify">
@@ -36,24 +30,31 @@ export default function Home() {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <div className="flex flex-wrap justify-center gap-4">
-            <Link href="/auth/signup">
-              <button className="bg-[#7300ff] text-white rounded-lg w-40 h-12 shadow-md transition-transform duration-300 hover:scale-105">
-                Register
-              </button>
-            </Link>
-            <Link href="/home">
-              <button className="bg-[#7300ff] text-white rounded-lg w-40 h-12 shadow-md transition-transform duration-300 hover:scale-105">
-                Home
-              </button>
-            </Link>
+          <div className="flex flex-wrap  gap-4">
+            <button
+              onClick={handleNavigation("/auth/signup")}
+              className="border border-[#7300ff] text-[#7300ff] font-medium rounded-lg w-32 h-10 shadow-md transition-transform duration-300 hover:scale-105 bg-[#e1d6fe]  cursor-pointer"
+            >
+              Register
+            </button>
+            <button
+              onClick={handleNavigation("/home")}
+              className="bg-[#7300ff] text-white rounded-lg w-32 h-10 shadow-md transition-transform duration-300 hover:scale-105  cursor-pointer"
+            >
+              Home
+            </button>
           </div>
         </div>
-        <div className="w-1/3 relative">
+        <div className="lg:w-1/3 w-full relative p-5 md:p-2 items-center flex justify-center">
+          <Image
+            src={rose}
+            alt="Begenra Institute Background"
+            className="h-72 w-44 translate-4 rounded-lg"
+          />
           <Image
             src={heroBackground}
             alt="Begenra Institute Background"
-            className="h-72 w-52"
+            className="h-72 w-44 rounded-lg"
           />
         </div>
       </div>
