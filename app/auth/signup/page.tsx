7bdc79a984d113af07d693103341967d36e35139
BettyyN <betelhemnegash10@gmail.com>
2025-04-29 21:05:44 +0300

    sign up page config

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -7,14 +7,14 @@ export default function page() {
       <div className="bg-slate-50 min-h-screen">
         <Header />
         <div className=" flex lg:flex-row flex-col">
-          <div className="lg:w-2/5 w-full">
+          <div className="lg:w-2/5 w-full flex justify-center items-center">
             <img
-              src="/signup.png"
+              src="/signup.svg"
               alt="signup"
               className=" lg:w-[410px] lg:h-[410px] lg:top-3/5  z-10"
             />
           </div>
-          <div className="lg:w-3/5 w-full  shadow-primary shadow-lg rounded-tl-3xl rounded-bl-3xl mb-10 mr-18 opacity-95">
+          <div className="lg:w-2/5 w-full  shadow-[#e1d6fe] shadow-lg rounded-tl-3xl rounded-bl-3xl px-5">
             <SignupForm />
           </div>
         </div>
