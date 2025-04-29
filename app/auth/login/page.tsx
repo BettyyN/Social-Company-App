0b978993758892e81e087f0c1a7cf296dfb9bbb2
BettyyN <betelhemnegash10@gmail.com>
2025-04-29 21:05:27 +0300

    login page config

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/Components/loginForm";
 export default function page() {
   return (
     <>
-      <div className="bg-slate-100 min-h-screen ">
+      <div className="bg-[#F5F7FA] min-h-screen ">
         <Header />
         <div className="flex lg:flex-row flex-col  items-center mt-20">
           <div className="lg:w-1/3 w-full">
@@ -14,7 +14,7 @@ export default function page() {
               className="lg:absolute lg:w-[500px] lg:h-[500px] lg:top-1/2 lg:left-1/6 transform lg:-translate-x-1/3 lg:-translate-y-1/2"
             />
           </div>
-          <div className="lg:w-1/3 w-full rounded-tl-3xl rounded-bl-3xl shadow-primary shadow-lg">
+          <div className="lg:w-1/3 w-full rounded-tr-3xl rounded-br-3xl shadow-[#7300ff] shadow-sm bg-white]">
             <LoginForm />
           </div>
         </div>
