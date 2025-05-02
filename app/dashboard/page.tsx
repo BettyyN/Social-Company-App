bdacef52692b23eb5d5b48535047a9142880cd60
BettyyN <betelhemnegash10@gmail.com>
2025-05-02 20:49:01 +0300

    page for dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+
+function page() {
+  return (
+    <>
+      <div className="w-full ">
+        <button>Create New Group</button>
+        <ul>
+          <li>
+            <a>groupName</a>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default page
