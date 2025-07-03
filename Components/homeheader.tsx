8b892fee07196df8cab4e23ef8c4ad5977bc48ab
BettyyN <betelhemnegash10@gmail.com>
2025-07-03 19:49:58 +0300

    group creation,profile view, sign up

--- Components/homeheader.tsx
+++ Components/homeheader.tsx
@@ -1,22 +1,19 @@
 "use client"
 import Link from "next/link";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 import { Bell, MessageCircle } from "lucide-react";
-import { signOut } from "next-auth/react";
-import { useState } from "react";
-import Profile from "./Profile";
+import {signOut } from "next-auth/react";
 
-export default function HomeHeader({ userName }: { userName: string }) {
+export default function HomeHeader({ userName }: { userName: string }) 
+{
   return (
     <>
       <header className="shadow-sm bg-[#F4F0FF] text-primary opacity-80 sticky top-0 z-50  rounded-2xl flex">
         <div className="text-sm w-1/3 md:w-1/4 p-5 md:text-lg">
           <span className="flex">Selam, {userName}</span>
         </div>
-        <div className="container flex items-center  pl-40  py-2">
+        <div className="container flex items-center justify-end px-3  py-2">
           {/* Desktop Menu */}
-          <div className="flex flex-row md:space-x-10 text-primary space-x-3">
+          <div className="flex flex-row md:space-x-5 text-primary space-x-3">
             <div className="pt-2">
               <MessageCircle className="w-5 h-5 md:w-7 md:h-7" />
             </div>
