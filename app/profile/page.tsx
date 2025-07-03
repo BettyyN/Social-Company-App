b95cb0b02d7e34e5bade82e1b3a49eddfee205e3
BettyyN <betelhemnegash10@gmail.com>
2025-07-03 19:49:29 +0300

    group creation,profile view, sign up

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,21 +1,31 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 import ProfileCover from "@/Components/profileCover";
 import ProfileAbout from "@/Components/profileAbout";
 import ProfilePost from "@/Components/profilePost";
+import { db } from "@/lib/db";
 
-function page() {
+export default async function ProfilePage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.userId) return <div>You must be logged in</div>
+
+  const user = await db.user.findUnique({
+    where: { userId: parseInt(session.user.userId) },
+  });
+
+  if (!user) return <div>User not found</div>;
+
   return (
     <>
-      <ProfileCover />
+      <ProfileCover user={user} />
       <div className="flex gap-3 m-3">
         <div className="ml-24">
-          <ProfileAbout />
+          <ProfileAbout user={user} />
         </div>
         <div className="pl-5 py-3 pr-24">
-          <ProfilePost />
+          <ProfilePost userId={user.userId}/>
         </div>
       </div>
     </>
   );
 }
-
-export default page
