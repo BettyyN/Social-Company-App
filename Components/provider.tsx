cfe447ff0ded459d79926ec3f4dbbe7a86da079d
BettyyN <betelhemnegash10@gmail.com>
2025-07-03 19:50:37 +0300

    group creation,profile view, sign up

--- Components/provider.tsx
+++ Components/provider.tsx
@@ -1,13 +1,17 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 
 interface ProviderProps{
     children: ReactNode;
 }
 
 const provider: FC<ProviderProps> = ({children}) => {
-  return <SessionProvider>{children}</SessionProvider>
+  const [queryClient] = useState(() => new QueryClient());
+  return <SessionProvider> <QueryClientProvider client={queryClient}>
+  {children}
+</QueryClientProvider></SessionProvider>
 };
 
 export default provider;
