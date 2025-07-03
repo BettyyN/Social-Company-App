b710d205e759a821da402a703d07a7b6b42373be
BettyyN <betelhemnegash10@gmail.com>
2025-07-03 19:49:15 +0300

    group creation,profile view, sign up

---
+++ app/group/page.tsx
@@ -0,0 +1,10 @@
+import CreateGroupPopup from "@/Components/group";
+import PopUp from "@/Components/Post/popUp";
+
+export default function page() {
+  return (
+    <div>
+        <CreateGroupPopup/>
+    </div>
+  )
+}
