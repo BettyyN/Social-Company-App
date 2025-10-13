43a5a858d517166b128f3e2a4b0461b6fc2ea67c
BettyyN <betelhemnegash10@gmail.com>
2025-10-13 12:34:29 +0300

    constants

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+
+// Auth tokens
+export const ACCESS_TOKEN_LIFETIME = 15 * 60 * 1000;
+export const REFRESH_TOKEN_LIFETIME = 60 * 60 * 24 * 30 * 1000;
+
+// API base URL 
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+
