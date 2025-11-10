7cbd3f83e133ea8a2b88297bad94a9f73a203e4c
BettyyN <betelhemnegash10@gmail.com>
2025-11-10 20:29:10 +0300

    ui change

---
+++ Components/group/CreateGroupForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateGroupForm: React.FC = () => {
+  return (
+    <div>
+      <h2>Create New Group</h2>
+      <p>This is the form for creating a new group.</p>
+      {/* Add form elements here */}
+    </div>
+  );
+};
+
+export default CreateGroupForm;
